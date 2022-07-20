a2f520c3f9e93bc875739c173ada208679be9288
Brandon Flores <brandonflores.ft@gmail.com>
2022-07-19 22:56:36 -0500

    refactor: in the main.js file the code was created to make the language transition

--- Desktop/practica/src/main.jsx
+++ Desktop/practica/src/main.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import { I18nextProvider } from "react-i18next";
+import { GlobalProvider } from "./Context/Global/global.context";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import i18n from "./config/localization/i18n";
+
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <I18nextProvider i18n={i18n}>
+      <GlobalProvider>
+        <App />
+      </GlobalProvider>
+    </I18nextProvider>
   </React.StrictMode>
-)
+);
